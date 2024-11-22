a76602fe80e2935b8e91d487671fcbeb4c8de2c0
mrigakshipandey <mrigakshipandey@gmail.com>
2024-11-22 23:00:07 +0100

    TopVar Fails

--- src/test/main_test.cpp
+++ src/test/main_test.cpp
@@ -15,13 +15,15 @@ class ManagerTest : public :: testing::Test {
     { 
         manager.createVar("a");
         manager.createVar("b");
+        manager.createVar("c");
+        manager.createVar("d");
     }
 };
 
 TEST_F(ManagerTest, CreateVar){
     
-    EXPECT_EQ(manager.createVar("c"), 4);
-    EXPECT_EQ(manager.createVar("d"), 5);
+    EXPECT_EQ(manager.createVar("test_var1"), 6);
+    EXPECT_EQ(manager.createVar("test_var2"), 7);
 
 }
 
@@ -30,6 +32,8 @@ TEST_F(ManagerTest, IsConstant) {
   EXPECT_TRUE(manager.isConstant(1));
   EXPECT_FALSE(manager.isConstant(2));
   EXPECT_FALSE(manager.isConstant(3));
+  EXPECT_FALSE(manager.isConstant(4));
+  EXPECT_FALSE(manager.isConstant(5));
 }
 
 TEST_F(ManagerTest, IsVariable) {
@@ -37,6 +41,8 @@ TEST_F(ManagerTest, IsVariable) {
   EXPECT_FALSE(manager.isVariable(1));
   EXPECT_TRUE(manager.isVariable(2));
   EXPECT_TRUE(manager.isVariable(3));
+  EXPECT_TRUE(manager.isVariable(4));
+  EXPECT_TRUE(manager.isVariable(5));
 }
 
 TEST_F(ManagerTest, True) { EXPECT_EQ(manager.True(), 1); }
@@ -44,9 +50,16 @@ TEST_F(ManagerTest, True) { EXPECT_EQ(manager.True(), 1); }
 TEST_F(ManagerTest, False) { EXPECT_EQ(manager.False(), 0); }
 
 TEST_F(ManagerTest, UniqueTableSize) {
-    manager.createVar("a");
-    manager.createVar("b");
-    EXPECT_EQ(manager.uniqueTableSize(), 4); 
+    EXPECT_EQ(manager.uniqueTableSize(),6); 
+}
+
+TEST_F(ManagerTest, TopVar){
+    EXPECT_EQ(manager.topVar(manager.False()), 0);
+    EXPECT_EQ(manager.topVar(manager.True()), 1);
+    EXPECT_EQ(manager.topVar(2), 2);
+    EXPECT_EQ(manager.topVar(3), 3);
+    EXPECT_EQ(manager.topVar(4), 4);
+    EXPECT_EQ(manager.topVar(5), 5);
 }
 
 TEST_F(ManagerTest, topVarName) { 
