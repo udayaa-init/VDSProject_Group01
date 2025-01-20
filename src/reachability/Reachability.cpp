0b70b0c0e2325e6aac2fd422ea094bc4e14e6568
Udaya Raj Subedi <075bei047.udaya@pcampus.edu.np>
2025-01-20 21:20:58 +0100

    Adding vectos for sttes, inputs, nextstate, initial state and transiston function and implementing basic functions

--- src/reachability/Reachability.cpp
+++ src/reachability/Reachability.cpp
@@ -1,1 +1,51 @@
 #include "Reachability.h"
+
+namespace ClassProject{
+
+    Reachability::Reachability(unsigned int stateSize, unsigned int inputSize): 
+      states(stateSize, 0),
+      inputs(inputSize, 0),
+      nextStates(stateSize, 0),
+      initState(stateSize, 0),
+      transitionFunctions(stateSize, 0),
+      ReachabilityInterface::ReachabilityInterface(stateSize, inputSize){
+        if(stateSize <= 0)
+            throw std::runtime_error(" !!!!!stateSize must be greater than zero !!!!!!");
+        // create current state, next state, and input variables
+        for (unsigned int i = 0; i < stateSize; i++) {
+            transitionFunctions[i] = states[i] = createVar(std::to_string(i));
+            nextStates[i] = createVar(std::to_string(i));
+        }
+
+        for (unsigned int i = 0; i < inputSize; i++) {
+            inputs[i] = createVar(std::to_string(i));
+        }
+    }    
+
+    const std::vector<BDD_ID> &Reachability::getStates() const{
+        return states;
+    }
+
+    const std::vector<BDD_ID> &Reachability::getInputs() const{
+        return inputs;
+    }
+
+    bool Reachability::isReachable(const std::vector<bool> &stateVector){
+        throw std::runtime_error("pureVirtualFunction is not implemented yet.");
+    }
+
+    int Reachability::stateDistance(const std::vector<bool> &stateVector){
+        throw std::runtime_error("pureVirtualFunction is not implemented yet.");
+    }
+    void Reachability::setTransitionFunctions(const std::vector<BDD_ID> &transitionFunctions){
+        throw std::runtime_error("pureVirtualFunction is not implemented yet.");
+    }
+
+    void Reachability::setInitState(const std::vector<bool> &stateVector){
+        if(stateVector.size() != states.size())
+            throw std::runtime_error("Vector size does not match state variables");
+
+        for(int i=0; i<stateVector.size(); i++){
+            this->initState[i] = stateVector[i] ? True() : False();
+    }
+}
--- src/reachability/Reachability.h
+++ src/reachability/Reachability.h
@@ -7,35 +7,30 @@ namespace ClassProject {
 
    class Reachability : public ReachabilityInterface {
 
+    private:
+    std::vector<BDD_ID> states;
+    std::vector<BDD_ID> inputs;
+    std::vector<BDD_ID> nextStates;
+    std::vector<BDD_ID> initState;
+    std::vector<BDD_ID> transitionFunctions;
     // Adding place holder for pure virtual functions
     public:
+    explicit Reachability(unsigned int stateSize, unsigned int inputSize = 0);
     Reachability(int param) : ReachabilityInterface(param) {
         // Derived class constructor body
     }
 
-    std::vector<BDD_ID> &getStates() const override {
-        throw std::runtime_error("pureVirtualFunction is not implemented yet.");
-    }
+    const std::vector<BDD_ID> &getStates() const override ;
 
-    std::vector<BDD_ID> &getInputs() const override {
-        throw std::runtime_error("pureVirtualFunction is not implemented yet.");
-    }
+    const std::vector<BDD_ID> &getInputs() const override;
 
-    bool isReachable(const std::vector<bool> &stateVector) override {
-        throw std::runtime_error("pureVirtualFunction is not implemented yet.");
-    }
+    bool isReachable(const std::vector<bool> &stateVector) override;
 
-    int stateDistance(const std::vector<bool> &stateVector) override {
-        throw std::runtime_error("pureVirtualFunction is not implemented yet.");
-    }
+    int stateDistance(const std::vector<bool> &stateVector) override;
 
-    void setTransitionFunctions(const std::vector<BDD_ID> &transitionFunctions) override {
-        throw std::runtime_error("pureVirtualFunction is not implemented yet.");
-    }
+    void setTransitionFunctions(const std::vector<BDD_ID> &transitionFunctions) override;
 
-    void setInitState(const std::vector<bool> &stateVector) override {
-        throw std::runtime_error("pureVirtualFunction is not implemented yet.");
-    }
+    void setInitState(const std::vector<bool> &stateVector) override;
 
    };
 
